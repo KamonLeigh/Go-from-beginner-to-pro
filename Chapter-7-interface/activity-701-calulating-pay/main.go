cfcd3f6973f9a50ae3261c9c697a304bcdf965c5
Byron Dunkley <byronleigh80@outlook.com>
2026-05-30 11:36:11 +0100

    Switch to Person field for Developer and Manager

--- Chapter-7-interface/activity-701-calulating-pay/main.go
+++ Chapter-7-interface/activity-701-calulating-pay/main.go
@@ -37,10 +37,6 @@ func main() {
 	employeeReview["Problem-solving"] = 4
 	employeeReview["Dependability"] = "Unsatisfactory"
 
-	// d := Developer{Individual: Employee{Id: 1, FirstName: "Eric", LastName: "Davis"}, HourlyRate: 35, HoursWorkedInYear: 2400, Review: employeeReview}
-	// 	m := Manager{Individual: Employee{Id: 2, FirstName: "Mr.", LastName: "Boss"}, Salary: 150000, CommissionRate: .07}
-	// 	err := d.ReviewRating()
-
 	d := Developer{Person: Employee{Id: 1, FirstName: "Eric", LastName: "Davis"}, HourlyRate: 35, HoursWorkedInYear: 2400, Review: employeeReview}
 	m := Manager{Person: Employee{Id: 2, FirstName: "Mr", LastName: "Boss"}, Salary: 150000, CommissionRate: .07}
 
